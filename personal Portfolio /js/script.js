71a2773958e2bf91d7f9a7620eb5a8aea40dfb06
hamada017 <113203043+hamada017@users.noreply.github.com>
2023-07-12 14:40:21 +0530

    yes

--- personal Portfolio /js/script.js
+++ personal Portfolio /js/script.js
@@ -1,21 +1,25 @@
 /*========== menu icon navbar ==========*/
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
 
 
 /*========== scroll sections active link ==========*/
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
+// window.onscroll = () => {
 
 
     /*========== sticky navbar ==========*/
+    window.onscroll = () =>{
+let header = document.querySelector('.header');
 
+header.classList.toggle('sticky', window.scrollY > 100);
+    };
 
     /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
 
-};
+
 
 
 /*========== swiper ==========*/

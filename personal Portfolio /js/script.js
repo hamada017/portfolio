5b514dabe7a50122c69174d68c93a066294b8ebc
hamada017 <113203043+hamada017@users.noreply.github.com>
2023-07-13 06:08:58 +0530

    y

--- personal Portfolio /js/script.js
+++ personal Portfolio /js/script.js
@@ -1,13 +1,12 @@
 /*========== menu icon navbar ==========*/
-// let menuIcon = document.querySelector('#menu-icon');
-// let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector('#menu-icon');
+ let navbar = document.querySelector('.navbar');
 
 
 /*========== scroll sections active link ==========*/
-// let sections = document.querySelectorAll('section');
-// let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-// window.onscroll = () => {
 
 
     /*========== sticky navbar ==========*/

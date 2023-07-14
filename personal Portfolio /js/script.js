0de31ff2c71d3cb24c9d1cb59b356a0de3855504
hamada017 <113203043+hamada017@users.noreply.github.com>
2023-07-14 14:14:42 +0530

    new

--- personal Portfolio /js/script.js
+++ personal Portfolio /js/script.js
@@ -23,6 +23,35 @@ header.classList.toggle('sticky', window.scrollY > 100);
 
 /*========== swiper ==========*/
 
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     loop: true,
+//     pagination:{
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grapCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
 
 /*========== dark light mode ==========*/
 

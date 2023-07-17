757a4dc4cdd94c5b78dd25bafb3e85d0ad1ad88b
hamada017 <113203043+hamada017@users.noreply.github.com>
2023-07-17 15:58:38 +0530

    y

--- personal Portfolio/connection.php
+++ personal Portfolio/connection.php
@@ -3,16 +3,48 @@
     $username = "root";
     $password = "root";
     $database = "portfolio";
-
+    
+    
     //creation de connection
     $connection = new mysqli($servername, $username, $password, $database);
 
     //verification de la connection
     if ($connection->connect_error){
         die("connection failed: ". $connection->connect_error);}
-        else{
-            echo "congrat";
-        }
+       
+
+            
+//    $Name = $_POST['Name'];
+//    $Email = $_POST['Email'];
+//    $Number = $_POST['Number'];
+//    $subject = $_POST['subject'];
+//    $text = $_POST['text'];
+
+   //connexion au base de donnee
+   $conn = new mysqli('localhost','root','root','portfolio');
+   if($conn->connect_error){
+    die('Connection Failed : '.$conn->connect_error);
+   }else{
+       
+
+
+   if (isset($_POST['submit'])) {
+    //code...
+    $Name=$_POST['Name'];
+    $Email=$_POST['Email'];
+    $Number=$_POST['Number'];
+    $subject=$_POST['subject'];
+    $text=$_POST['text'];
+
+  $sql="insert into cv (Name,Email,Number,subject,text) values ('$Name', '$Email', '$Number', '$subject', '$text')";
+  $result=mysqli_query($connection,$sql);
+  if($result){
+  echo "connecté";
+  }else{
+  echo "echec";
+}
+}
+   }
 ?>
 
 
@@ -32,17 +64,17 @@
      <section class="contact" id="contact">
         <h2 class="heading">Contact <span>Me!</span></h2>
 
-        <form action="#">
+        <form action="#" method="post">
             <div class="input-box">
-               <input type="text" placeholder="Full Name">
-               <input type="email" placeholder="Email Address">
+               <input type="text" placeholder="Full Name" name="Name">
+               <input type="email" placeholder="Email Address" name="Email">
             </div>
             <div class="input-box">
-                <input type="number" placeholder="Mobile Number">
-                <input type="text" placeholder="Email Subject">
+                <input type="number" placeholder="Mobile Number" name="Number">
+                <input type="text" placeholder="Email Subject" name="subject">
             </div>
-            <textarea name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
-            <input type="submit" value="Send Message" class="btn">
+            <textarea name="text" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
+            <input type="submit" value="Send Message" class="btn" name="submit">
         </form>
     </section>
 </body>
